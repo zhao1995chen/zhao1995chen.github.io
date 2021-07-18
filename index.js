12ced06857149f435af5ce78c844e7f791fe6368
zhaowenchen <zhao1995.chen@gmail.com>
2021-07-19 01:38:30 +0800

    refactor: adjust page feat: add Vue3 to project list

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 (function () {
+  const experience = document.querySelectorAll('.experience');
   const cards = document.querySelectorAll('.card');
 
   function moveHandler() {}
 
+  function experienceHandler(e) {
+    console.log(e);
+    const list = e.currentTarget.querySelector('.description ul');
+    console.log(list);
+    const method = e.type === 'click' ? 'toggle' : 'show';
+    list.classList[method]('d-none');
+  }
   cards.forEach((card) => {
     card.addEventListener('mousemove', moveHandler);
+  });
+
+  experience.forEach((e) => {
+    // e.addEventListener('mouseenter', experienceHandler);
+    e.addEventListener('click', experienceHandler);
   });
 })();
